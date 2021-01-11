77f385e5ba6c81092801e115ae4a39b852d155d1
Agboolaidris <agboolaisholaidreez@gmail.com>
2021-01-11 14:08:24 +0100

    update files

--- src/Component/Auth/Login/Login.jsx
+++ src/Component/Auth/Login/Login.jsx
@@ -1,18 +1,29 @@
 import React from "react";
 import Left_page from "./Left_Page";
 import Right_page from "./Right_page";
+import { connect } from "react-redux";
 
-function Login() {
+function Login({ isloading }) {
   return (
-    <div className="login">
-      <div className="col-1">
-        <Left_page />
+    <>
+      <div className={isloading ? "loading" : "loading-none"}>
+        <span></span>
       </div>
-      <div className="col-2">
-        <Right_page />
+      <div className={isloading ? "login loading-opacity" : "login"}>
+        <div className="col-1">
+          <Left_page />
+        </div>
+        <div className="col-2">
+          <Right_page />
+        </div>
       </div>
-    </div>
+    </>
   );
 }
+const mapStateToProps = (state) => {
+  return {
+    isloading: state.Auth.isLoading,
+  };
+};
 
-export default Login;
+export default connect(mapStateToProps)(Login);
--- src/Component/Auth/Register/Register.jsx
+++ src/Component/Auth/Register/Register.jsx
@@ -5,17 +5,19 @@ import { connect } from "react-redux";
 
 function Register({ isloading }) {
   return (
-    <div className="register">
+    <>
       <div className={isloading ? "loading" : "loading-none"}>
         <span></span>
       </div>
-      <div className="col-1">
-        <Left_page />
+      <div className={isloading ? "loading-opacity register" : "register"}>
+        <div className="col-1">
+          <Left_page />
+        </div>
+        <div className="col-2">
+          <Right_page />
+        </div>
       </div>
-      <div className="col-2">
-        <Right_page />
-      </div>
-    </div>
+    </>
   );
 }
 const mapStateToProps = (state) => {

952c1a9ad259798f7f2a3c97512399d2eafc22fb
Agboolaidris <agboolaisholaidreez@gmail.com>
2021-01-11 08:58:27 +0100

    update files

--- src/Store.js
+++ src/Store.js
@@ -1,9 +1,10 @@
 import {createStore, compose,applyMiddleware} from 'redux'
 import thunk from 'redux-thunk'
 import RootReducer from './Reducer/RootReducer'
-import {composeWithDevTools} from 'redux-devtools-extension' 
+import {composeWithDevTools} from 'redux-devtools-extension'
 
-const composeEnhancers = [applyMiddleware(thunk),composeWithDevTools()]
+ const REDUX_DEV_TOOLS = window._REDUX_DEVTOOLS_EXTENSION_ && composeWithDevTools()
+ const composeEnhancers = [applyMiddleware(thunk),REDUX_DEV_TOOLS]
 
 const Store = createStore(RootReducer,compose(...composeEnhancers))
 

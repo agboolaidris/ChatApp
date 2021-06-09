cebf3f49a5916852b9f5192d77a909fc3b20d9f6
idris agboola <agboolaisholaidreez@gmail.com>
2021-06-09 07:48:48 +0100

    update

--- server/build/config/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.ENV = void 0;
-exports.ENV = {
-    JWT_SECRET: "36sWhfYmB4QLcVcvGyrSqNnV34fE3D8SGPkJSrJ45byb24Sj99Z9eHhCHHQbcRWmjrDqhspvRBWpQVPTRVgeAZ",
-    NODE_ENV: "prod",
-    MONGODB_URL: "mongodb+srv://chatMe:8fX7O9PEEL7UZVh8@cluster0.ugtzp.mongodb.net/Database?retryWrites=true&w=majority",
-};
-//# sourceMappingURL=index.js.map
